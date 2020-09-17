d27c03583b1843715f37ba47a43e23b8e78060c0
mhans003 <michaeledwardhanson@gmail.com>
2020-09-16 20:21:17 -0700

    Moved the call to the requestYelp function out of the locationRetrieved function and into its own event listener on the search input div. When the user clicks the search div, a call to the requestYelp function occurs. The requestYelp function now checks if the event caller contains search items with a length greater than 0, and if there is search content, it appends the search to the end of the query string. It also checks if the local coordinates have been retrived, and if so, appends to the query string; Added a throw/catch to see if the Yelp request fails. If so, it calls Zomato instead; Pasted categories array of objects in and added logic for generating drop down menus in html.

--- index.js
+++ index.js
@@ -2,13 +2,178 @@
 var latitude; 
 var longitude; 
 
+//Define categories for the drop down menu. 
+var categories = [
+    {
+        main: "fitness",
+        sub: [
+            {
+                main: "gym",
+                sub: [
+                    {
+                        main: "power lifting",
+                        sub: [
+                            {
+                                main: "Eos"
+                            },
+                            {
+                                main: "Crossfit"
+                            },
+                            {
+                                main: "Golds"
+                            }
+                        ]
+                    }, 
+                    {
+                        main: "club gym",
+                        sub: [
+                            {
+                                main: "la fitness"
+                            },
+                            {
+                                main: "24 hour fitness"
+                            },
+                            {
+                                main: "Planet Fitness"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                main: "studio",
+                sub: [
+                    {
+                        main: "yoga",
+                        sub: [
+                            {
+                                main: "heat yoga"
+                            }
+                        ]
+                    }
+                ]
+            }
+            
+        ]
+    },
+    {
+        main: "grocery",
+        sub: [
+            {
+                main: "natural"
+            }
+        ]
+    },
+    {
+        main: "restaurants",
+        sub: [
+            {
+                main: "vegetarian"
+            },
+            {
+                main: "paleo"
+            },
+            {
+                main: "vegan",
+                sub: [
+                    {
+                        main: "raw vegan"
+                    }
+                ]
+            }
+        ]
+    }
+]; 
+
 //Define HTML variables.
 var output = document.querySelector("#generated-content"); 
+var searchIcon = document.querySelector("#search-icon"); 
+
+//Generate the main drop down menu.
+generateDropdown(); 
 
 //Get the user's local coordinates. 
 getLocalCoordinates(); 
 
 //Functions 
+
+function generateDropdown() {
+    //Generate main drop down items 
+    for(let i = 0; i < categories.length; i++) {
+        var main = document.createElement("div"); 
+        main.classList.add("item"); 
+        main.innerText = categories[i].main; 
+
+        document.querySelector("#main-dropdown").append(main); 
+
+        //Add event listeners to main categories
+        main.addEventListener("click", function(event) {
+            console.log(this.parentElement.parentElement); 
+
+            //Delete existing element siblings. 
+            while(this.parentElement.parentElement.nextElementSibling) {
+                this.parentElement.parentElement.nextElementSibling.remove(); 
+            }
+
+            //Generate sub-categories 
+            generateSub(categories[i]); 
+        }); 
+    }
+}
+
+function generateSub(category) {
+
+        //make new dropdown
+        var dropdown = document.createElement("div"); 
+        dropdown.classList.add("ui","simple","selection","dropdown"); 
+        var input = document.createElement("input");
+        input.setAttribute("type","hidden"); 
+        input.setAttribute("name", category.main); 
+
+        var icon = document.createElement("i"); 
+        icon.classList.add("dropdown","icon"); 
+
+        var categoryHeading = document.createElement("div"); 
+        categoryHeading.classList.add("default","text"); 
+        categoryHeading.innerText = category.main; 
+
+        var menu = document.createElement("div"); 
+        menu.classList.add("menu"); 
+
+        dropdown.appendChild(input); 
+        dropdown.appendChild(icon); 
+        dropdown.appendChild(categoryHeading); 
+        dropdown.appendChild(menu); 
+
+        //document.body.appendChild(dropdown);
+        document.getElementById("dropdown-container").appendChild(dropdown);  
+
+        //Create sub items 
+
+        for(let i = 0; i < category.sub.length; i++) {
+
+            var sub = document.createElement("div"); 
+            sub.classList.add("item"); 
+            sub.innerText = category.sub[i].main; 
+
+            menu.appendChild(sub); 
+
+            if(category.sub[i].sub) {
+                //repeat if another sub category. 
+                sub.addEventListener("click", function() {
+                    //Delete any other dropdowns that are siblings
+                    while(this.parentElement.parentElement.nextElementSibling) {
+                        this.parentElement.parentElement.nextElementSibling.remove(); 
+                    } 
+
+                    //Generate the correct sub categories 
+                    generateSub(category.sub[i]); 
+                }); 
+            }
+
+        }
+    
+}
 
 function getLocalCoordinates() {
     //Access the user's local coordinates from the navigator object. 
@@ -26,22 +191,38 @@ function locationRetrieved(position) {
     //HERE
 
     //In the meantime, call the function that makes the reqest once the coordinates load. 
-    requestYelp(); 
+    //requestYelp(); 
 }
 
 function requestYelp(event) {
     //This function makes a call to the Zomato API to retrieve locations. 
 
     /*Set up the Zomato API request - Archived
-    var APIKEY_Yelp = `3bcad67a3e4d710dc9a409b26169eb5f`
-    var yelpQuery = `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`; 
+    var APIKEY_Zomato = `3bcad67a3e4d710dc9a409b26169eb5f`
+    var zomatoQuery = `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`; 
     
     */
 
-   var APIKEY_Yelp = 'Qk7R-McIeM66BXlGnkM65vLQmQyOdr4RZV5uND7h_MBnXgfmfi04W5GE_O5FzhRIxuoeKsRq1U33b5J2NtMCfRvFqsQpW0hQSD00CW3NcGauIanNVqgPsxTgGm1eX3Yx';
-   var yelpQuery = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=10&categories=restaurants&latitude=${latitude}&longitude=${longitude}`;
-
     //Add code here to see if search input was used. If so, add search keywords to the query string. 
+    console.log(event.target.value.length); 
+
+
+
+    var APIKEY_Yelp = 'Qk7R-McIeM66BXlGnkM65vLQmQyOdr4RZV5uND7h_MBnXgfmfi04W5GE_O5FzhRIxuoeKsRq1U33b5J2NtMCfRvFqsQpW0hQSD00CW3NcGauIanNVqgPsxTgGm1eX3Yx';
+    var yelpQuery = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=10`;
+
+    //Checks to see if there is anything in the search input field. If so, add it to the end of the query string. 
+    if(event.target.value.length > 0) {
+        var search = event.target.value; 
+        yelpQuery += `&term=${search}`; 
+    }
+
+    //If the coordinates have loaded, add to the query string. 
+    if(latitude && longitude) {
+        yelpQuery += `&latitude=${latitude}&longitude=${longitude}`; 
+    }
+
+    console.log("Query String:" + yelpQuery); 
 
     //Make API call. 
     fetch(yelpQuery, {
@@ -49,7 +230,15 @@ function requestYelp(event) {
             Authorization: `Bearer ${APIKEY_Yelp}`,
         }
     })
-    .then(response => response.json())
+    .then(response => {
+
+        //See if the response is not OK from the Yelp API
+        if(!response.ok) {
+            throw new Error("Failed to retrieve Yelp results."); 
+        }
+
+        return response.json();
+    })
     .then(res => {
         console.log(res); 
         //Generate the results to output to the user. 
@@ -79,11 +268,33 @@ function requestYelp(event) {
         */
 
 
+    })
+    .catch(error => {
+        //Display the error in the console.
+        console.log(error); 
+
+        //Make the alternate API call - move this to function later. 
+        var APIKEY_Zomato = `3bcad67a3e4d710dc9a409b26169eb5f`
+        var zomatoQuery = `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`;
+
+        fetch(zomatoQuery, {
+            method: "GET",
+            headers: {
+                "user-key": APIKEY_Zomato
+            }
+        })
+        .then(zomatoResponse => zomatoResponse.json())
+        .then(zomatoRes => {
+            console.log(zomatoRes); 
+
+            //Display the alternative results from Zomato
+            outputZomatoResults(zomatoRes); 
+        }); 
     }); 
 
 }
 
-function outputResults(results) {
+function outputZomatoResults(results) {
 
     //Clear output. 
     output.innerHTML = ""; 
@@ -159,5 +370,5 @@ function outputYelpResults(results) {
 
 //Events
 
-//someButton.addEventListener("click", requestYelp); 
+searchIcon.addEventListener("click", requestYelp); 
 
